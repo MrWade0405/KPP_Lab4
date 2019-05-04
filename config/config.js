6086b5b7b2c350c90aaf5a175d5a2f0daa73acf4
MahlonaVitalii <maglona2013@gmail.com>
2019-05-04 16:48:29 +0300

    first commit

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  port: 8080,
+  dbURL: 'mongodb://localhost/articles',
+  dbOptions: {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+}
--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Posts from './views/PostsPage.vue'
+import NewPost from './views/NewPostPage'
+import EditPost from './views/EditPostPage'
 
 Vue.use(Router)
+
 
 export default new Router({
   mode: 'history',
@@ -16,10 +20,22 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'newPost',
+      component: NewPost
+    },
+    {
+      path: '/posts/:id',
+      name: 'editPost',
+      component: EditPost
     }
   ]
 })
